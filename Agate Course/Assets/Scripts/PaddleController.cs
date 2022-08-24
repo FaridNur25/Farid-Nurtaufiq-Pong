acda42afdc3c61c46eaa8901230786c10f618b4d
FaridNur25 <faridnur25taufiq@gmail.com>
2022-08-24 20:12:17 +0700

    Base Gameplay
    Ping-Pong Gameplay

---
+++ Agate Course/Assets/Scripts/PaddleController.cs
@@ -0,0 +1,34 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PaddleController : MonoBehaviour
+{
+    //input speed
+    public int speed;
+    //input atas bawah
+    public KeyCode Upkey;
+    public KeyCode downkey;
+    //rigbody
+    private Rigidbody2D rig;
+    void Start()
+    {
+        rig = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector2 movement = Vector2.zero;
+        // getinput
+        if (Input.GetKey(Upkey))
+        {
+            movement = Vector2.up * speed;
+        }
+        else if (Input.GetKey(downkey))
+        {
+            movement = Vector2.down * speed;
+        }
+        rig.velocity = movement;
+    }
+}
